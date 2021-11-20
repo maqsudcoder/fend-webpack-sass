60649c0df06b8d903e05ef84309523ae18cbb7ae
Maqsud <maqsudcoder@gmail.com>
2021-11-20 21:37:48 +0500

    change something

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,21 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
+  // check what text was put into the form field
+  let formText = document.getElementById('name').value;
 
-    Client.checkForName(formText)
+  Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
+  console.log('::: Form Submitted :::');
+  fetch('http://localhost:8081/test')
+    .then((res) => {
+      console.log('res is recieved');
+      return res.json();
     })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
-    })
+    .then(function (data) {
+      console.log('inner html ');
+      document.getElementById('results').innerHTML = data.message;
+    });
 }
 
-export { handleSubmit }
+export { handleSubmit };
